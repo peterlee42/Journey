53c1540a69eb64860d904babc92248b2165ca6b3
Peter Lee <76502486+peterlee42@users.noreply.github.com>
2024-06-08 12:13:19 -0400

    NFTs added

---
+++ Frontend/layers/content.js
@@ -0,0 +1,89 @@
+module.exports = {
+    layers: [
+        {
+            name: "Background",
+            probability: 1.0,
+            options: [
+                {
+                    name: "Blue",
+                    file: "Background/Blue.png",
+                    weight: 1
+                },
+                {
+                    name: "Black",
+                    file: "Background/Black.png",
+                    weight: 1
+                },
+                {
+                    name: "Orange",
+                    file: "Background/Orange.png",
+                    weight: 1
+                },
+                {
+                    name: "Yellow",
+                    file: "Background/Yellow.png",
+                    weight: 1
+                }
+            ]
+        },
+        {
+            name: "Cloud",
+            probability: 1.0,
+            options: [
+                {
+                    name: "Cloud",
+                    file: "Cloud/cloud.png",
+                    weight: 1
+                },
+                {
+                    name: "Cloud1",
+                    file: "Cloud/cloud1.png",
+                    weight: 1
+                },
+                {
+                    name: "Cloud2",
+                    file: "Cloud/cloud2.png",
+                    weight: 1
+                }
+            ]
+        },
+        {
+            name: "Tree",
+            probability: 1.0,
+            options: [
+                {
+                    name: "Tree",
+                    file: "Tree/tree.png",
+                    weight: 1
+                },
+                {
+                    name: "Tree1",
+                    file: "Tree/tree1.png",
+                    weight: 1
+                },
+                {
+                    name: "Tree2",
+                    file: "Tree/tree2.png",
+                    weight: 1
+                }
+            ]
+        },
+        {
+            name: "Bench",
+            probability: 1.0,
+            options: [
+                {
+                    name: "Bench1",
+                    file: "Bench/bench1.png",
+                    weight: 1
+                },
+                {
+                    name: "Bench2",
+                    file: "Bench/bench2.png",
+                    weight: 1
+                }
+            ]
+        }
+
+    ]
+};
